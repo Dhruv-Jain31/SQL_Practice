3e80c53b3cd18964e421b34ccb4f48357c1d0fc1
Dhruv-Jain31 <jain.dhruv3103@gmail.com>
2025-08-23 01:57:34 +0530

    added self joins

--- 3.querying_multiple_tables/1.multiple_table.sql
+++ 3.querying_multiple_tables/1.multiple_table.sql
@@ -48,3 +48,31 @@ SELECT emp.employeeNumber,
         mgr.jobTitle
 FROM employees emp
 JOIN employees mgr ON emp.reportsTo = mgr.employeeNumber;  -- here we are joining employees table with itself to get manager details
+
+
+-- LEFT JOIN example
+-- This query retrieves all customers along with their customets, including those who have not made any orders
+SELECT c.customerNumber,
+        customerName,
+        o.orderNumber,
+FROM customers c
+LEFT JOIN orders o ON c.customerNumber = o.customerNumber;
+
+-- RIGHT JOIN example
+-- This query retrieves all orders along with their customers, including those orders that do not have associated customers
+SELECT c.customerNumber,
+        customerNamee,
+        o.orderNumber,
+FROM customers c
+RIGHT JOIN orders 0 ON c.customerNumber = o.customerNumber;
+
+
+--SELF OUTER JOIN
+SELECT emp.employeeNumber,
+        emp.firstName,
+        emp.jobTitle,
+        mgr.firstName AS "Manager First Name",
+        mgr.jobTitle AS "Manager Job Title"
+FROM employees emp
+LEFT JOIN employees mgr ON emp.reportsTo = mgr.employeeNumber;  -- here we are joining employees table with itself to get manager details
+
