fdca55942fe0ccb0fac13e73ee3927aedc662072
Dhruv-Jain31 <jain.dhruv3103@gmail.com>
2025-08-23 02:02:08 +0530

    added all other joins

--- 3.querying_multiple_tables/1.multiple_table.sql
+++ 3.querying_multiple_tables/1.multiple_table.sql
@@ -68,6 +68,7 @@ RIGHT JOIN orders 0 ON c.customerNumber = o.customerNumber;
 
 
 --SELF OUTER JOIN
+-- This query retrieves all employees along with their managers, including employees who do not have a manager from the same table
 SELECT emp.employeeNumber,
         emp.firstName,
         emp.jobTitle,
@@ -76,3 +77,34 @@ SELECT emp.employeeNumber,
 FROM employees emp
 LEFT JOIN employees mgr ON emp.reportsTo = mgr.employeeNumber;  -- here we are joining employees table with itself to get manager details
 
+-- FULL OUTER JOIN example
+-- This query retrieves all customers and their orders, including customers without orders and orders without customers
+SELECT c.customerNumber,
+        customerName,
+        o.orderNumber
+FROM customers c
+FULL OUTER JOIN orders o ON c.customerNumber = o.customerNumber;
+
+--USING CLAUSE
+SELECT c.customerNumber,
+        customerName,
+        o.orderNumber
+FROM customers c
+FULL OUTER JOIN orders o USING (customerNumber); -- here we are using USING clause instead of ON clause to join tables on common column customerNumber
+
+-- CROSS JOIN example
+-- This query retrieves all possible combinations of customers and products
+SELECT c.customerNumber,
+        customerName,
+        p.productName
+FROM customers c
+CROSS JOIN products p;  -- here we are using CROSS JOIN to get all possible combinations 
+-- It does not require an ON clause because there's no matching condition; it simply generates all possible combinations.
+
+-- NATURAL JOIN example
+-- This query retrieves all customers along with their orders based on common columns
+SELECT c.customerNumber,
+        customerName,
+        o.orderNumber
+FROM customers c
+NATURAL JOIN orders o;  -- here we are using NATURAL JOIN to join tables
