fbc97728b10a32fc66bed41b2129c1134511a063
Dhruv-Jain31 <jain.dhruv3103@gmail.com>
2025-08-23 16:33:03 +0530

    added lexicograpgic comparison as well

--- 4. Data summarization/1.Aggregate_function.sql
+++ 4. Data summarization/1.Aggregate_function.sql
@@ -11,11 +11,24 @@ SELECT
     MIN(amount) AS "Minimum Amount",
     AVG(amount) AS "Average Amount",
     SUM(amount) AS "Total Amount",
-    COUNT(*) AS "Number of Payments"
+    COUNT(*) AS "Number of Payments"  -- counting total number of rows
 FROM payments;
 
 SELECT 
     customerNumber,
-    COUNT(orderNumber) AS "Total Orders",
+    COUNT(orderNumber) AS "Total Orders", -- counting total number of orders for each customer but it discards customers with no orders or null values
     SUM(quantityOrdered * priceEach) AS "Total Sales" -- calculating total sales by multiplying quantity ordered with price each
 FROM orderdetails;
+
+SELECT 
+    COUNT(*) FROM orders; -- counting total number of rows in orders table
+    WHERE status = 'Shipped'; -- counting total number of rows with status 'Shipped'
+FROM orders;
+
+
+SELECT MAX(paymentDate) AS "Last Payment Date"  -- getting the most recent payment date
+       MIN(paymentDate) AS "First Payment Date"  -- getting the earliest payment date
+FROM payments
+WHERE customerNumber = 103;  -- for a specific customer
+
+
